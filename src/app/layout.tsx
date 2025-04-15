9ba7e779ee0d97413b29d91becdd6cf7bcc23dbc
junjuny0227 <junjuny0227@gmail.com>
2025-04-15 11:12:32 +0900

    시스템 폰트로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,14 +13,6 @@ const RootLayout = ({
 }>) => {
   return (
     <html lang="ko">
-      <head>
-        <link
-          rel="stylesheet"
-          as="style"
-          crossOrigin=""
-          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable-dynamic-subset.css"
-        />
-      </head>
       <body>{children}</body>
     </html>
   );
